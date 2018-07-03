8c66d057d703d795341a9489b688ab9c3ad4c85d
Simon Pioli <simon@simonpioli.com>
2018-07-03 16:51:35 +0100

    Refactor to use Sass, updated media queries and other variables to use BS4 versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var sass = require('node-sass');
+
 module.exports = function (grunt) {
     'use strict';
     grunt.initConfig({
@@ -78,23 +80,26 @@ module.exports = function (grunt) {
                 config: '.jscs.json'
             }
         },
-        less: {
+        sass: {
             production: {
                 options: {
-                    cleancss: true,
-                    compress: true,
-                    paths: 'node_modules'
+                    implementation: sass,
+                    sourceMap: false,
+                    sourceComments: false,
+                    outputStyle: 'compressed',
+                    includePaths: ['node_modules']
                 },
                 files: {
-                    'build/css/bootstrap-datetimepicker.min.css': 'src/less/bootstrap-datetimepicker-build.less'
+                    'build/css/bootstrap-datetimepicker.min.css': 'src/sass/bootstrap-datetimepicker-build.scss'
                 }
             },
             development: {
                 options: {
-                    paths: 'node_modules'
+                    implementation: sass,
+                    includePaths: ['node_modules']
                 },
                 files: {
-                    'build/css/bootstrap-datetimepicker.css': 'src/less/bootstrap-datetimepicker-build.less'
+                    'build/css/bootstrap-datetimepicker.css': 'src/sass/bootstrap-datetimepicker-build.scss'
                 }
             }
         },
@@ -158,20 +163,20 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-nuget');
 
     require('load-grunt-tasks')(grunt);
-    grunt.registerTask('default', ['jshint', 'jscs', 'less', 'env:paris', 'connect', 'jasmine']);
+    grunt.registerTask('default', ['jshint', 'jscs', 'sass', 'env:paris', 'connect', 'jasmine']);
     grunt.registerTask('build:travis', [
         // code style
         'jshint', 'jscs',
         // build
-        'uglify', 'less',
+        'uglify', 'sass',
         // tests
         'env:paris', 'connect', 'jasmine'
     ]);
 
     // Task to be run when building
-    grunt.registerTask('build', ['jshint', 'jscs', 'uglify', 'less']);
+    grunt.registerTask('build', ['jshint', 'jscs', 'uglify', 'sass']);
 
-    grunt.registerTask('test', ['jshint', 'jscs', 'uglify', 'less', 'env:paris', 'connect', 'jasmine']);
+    grunt.registerTask('test', ['jshint', 'jscs', 'uglify', 'sass', 'env:paris', 'connect', 'jasmine']);
 
     grunt.registerTask('docs', 'Generate docs', function () {
         grunt.util.spawn({
